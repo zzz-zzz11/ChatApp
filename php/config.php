ca8d889e1465ca6967e2b3d6b3dc4c46edaf1e2e
caicaitong <lyt20220613@qq.com>
2025-06-28 14:46:54 +0800

    four commit

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
   $hostname = "localhost";
   $username = "root";
-  $password = "";
+  $password = "123456";
   $dbname = "ihello";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
--- 连接数据库.php
+++ 连接数据库.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "123456";
 $dbname = "ihello";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
